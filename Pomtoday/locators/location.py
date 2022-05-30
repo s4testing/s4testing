df555823f607a09f5464f136729734604d63b435
s4testing <anitupreti@gmail.com>
2022-05-30 15:06:41 +0100

    locators

---
+++ Pomtoday/locators/location.py
@@ -0,0 +1,11 @@
+class locators2:
+    url = 'https://demoblaze.com'
+    loginId = 'login2'
+    username = '//*[@id="loginusername"]'
+    password = '//*[@id="loginpassword"]'
+    button = '//*[@id="logInModal"]/div/div/div[3]/button[2]'
+    contact = "//*[@class='navbar-collapse']/ul/li[2]/a"
+    emailId = "recipient-email"
+    recepientName = "recipient-name"
+    textMessage = "message-text"
+    submitButton = "//*[@id='exampleModal']/div/div/div[3]/button[2]"
